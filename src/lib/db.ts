fd7ee85f0ef3a85a708bcfc62c1c1021719a8fca
masrur2234 <78709459+masrur2234@users.noreply.github.com>
2026-04-08 10:19:58 +0700

    Update db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,16 +1,22 @@
 import { PrismaClient } from '@prisma/client'
 import path from 'path'
+import fs from 'fs'
+
+const DB_PATH = path.join(process.cwd(), 'prisma', 'dev.db')
+
+// Pastikan file database ada
+if (!fs.existsSync(DB_PATH)) {
+  fs.writeFileSync(DB_PATH, '')
+}
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-const dbPath = path.join(process.cwd(), 'prisma', 'dev.db')
-
 export const db =
   globalForPrisma.prisma ??
   new PrismaClient({
-    datasourceUrl: `file:${dbPath}`
+    datasourceUrl: `file:${DB_PATH}`
   })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
